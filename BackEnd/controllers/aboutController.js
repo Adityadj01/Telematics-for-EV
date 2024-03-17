22a17abc9e08c740cb3be9c7ca4a0663a197aa1d
AshwiniJannu <01fe20bcs208@kletech.ac.in>
2024-03-17 16:43:38 +0530

    upload

--- BackEnd/controllers/aboutController.js
+++
@@ -1,7 +0,0 @@
-// controllers/aboutController.js
-
-// Function to handle GET request to the about page
-exports.getAboutPage = (req, res) => {
-    // Render the about page template (about.pug)
-    res.render('about', { title: 'About Page' });
-};
