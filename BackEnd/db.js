3675cc7edaec9ec9f4c509e9a46f5a9206ad47d1
AshwiniJannu <01fe20bcs208@kletech.ac.in>
2024-03-20 16:04:40 +0530

    Update db.js

--- BackEnd/db.js
+++ BackEnd/db.js
@@ -7,6 +7,11 @@ async function mongoDB() {
             useUnifiedTopology: true
         });
         console.log('Connected to MongoDB');
+        const fetched data = awaitmongoose.connection.db.collection("mycollection");
+        fetched_data.find({}).toArray(function(err,data){
+            if(err) console.log(err);
+            else console.log(data);
+        })
     } catch (error) {
         console.error('Error connecting to MongoDB:', error.message);
     }
