fd0a7d3a72e8c6981d20a43c776933e79b4ca1b1
AshwiniJannu <01fe20bcs208@kletech.ac.in>
2024-03-21 14:56:25 +0530

    Update config.js

--- BackEnd/config/config.js
+++ BackEnd/config/config.js
@@ -2,10 +2,10 @@
 
 module.exports = {
     // MongoDB connection settings
-    mongoURI: 'mongodb://localhost:27017/ev-website-db',
+    mongoURI: 'mongodb://localhost:27017/mydatabase',
     
     // JWT secret key for authentication
-    jwtSecret: 'your_jwt_secret_key_here',
+    // jwtSecret: 'your_jwt_secret_key_here',
     
     // Other configuration settings...
   };
