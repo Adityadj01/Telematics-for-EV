50487c80c5b0991c86c6d559d4583f4b378e42ef
Aditya Jadhav <97619691+Adityadj01@users.noreply.github.com>
2024-09-11 19:18:59 +0530

    Update index.ts

--- Server/index.ts
+++ Server/index.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({origin: 'http://localhost:5173', methods: ["GET", "POST", "PUT", "DELETE"],}))
+app.use(cors({origin: 'https://evdiagnose.vercel.app/', methods: ["GET", "POST", "PUT", "DELETE"],}))
 
 app.use(express.json());
 
